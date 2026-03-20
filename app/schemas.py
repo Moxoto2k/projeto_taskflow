9d1c9d31fb17ba54c5b41fb59ee0c5e2082e2fd0
Felipe <fgm@cesar.school>
2026-03-20 16:54:06 -0300

    schemas

---
+++ app/schemas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class UserCreate(BaseModel):
+    username: str
+    email: EmailStr
+    password: str
+
+class UserLogin(BaseModel):
+    username: str
+    password: str
+
+class TaskCreate(BaseModel):
+    title: str
+    description: Optional[str] = None
+
+class TaskUpdate(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
+    completed: Optional[bool] = None
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    description: Optional[str]
+    completed: bool
+    owner_id: int
+
+    class Config:
+        from_attributes = True
