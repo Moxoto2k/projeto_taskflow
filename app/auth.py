0d42d27657a90a1451d4777e24b0d17893df1b4e
Felipe <fgm@cesar.school>
2026-03-20 18:08:36 -0300

    listagem de tarefas autenticadas

--- app/auth.py
+++ app/auth.py
@@ -1,17 +1,16 @@
-from fastapi import Header, HTTPException
+from fastapi import HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 fake_tokens = {}
+security = HTTPBearer()
 
 def create_token(username: str):
     token = f"token-{username}"
     fake_tokens[token] = username
     return token
 
-def get_current_user(authorization: str = Header(None)):
-    if not authorization:
-        raise HTTPException(status_code=401, detail="Token não enviado")
-
-    token = authorization.replace("Bearer ", "")
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
     username = fake_tokens.get(token)
 
     if not username:
